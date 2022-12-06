153574b3b6614862d150bf71d9ccde38927489c6
Jose Toro <ing.josetoro.2@gmail.com>
2022-12-05 19:45:39 -0500

    Button Styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,40 @@
-import Button from "../components/button/Button";
+import Button from '../components/button/Button';
+import { BsArrowRightCircle } from 'react-icons/bs';
 
 function App() {
 	return (
 		<>
 			<Button>Click here</Button>
-			<Button className='btn--primary'>Click here</Button>
-			<Button className='btn--primary-40'>Click here</Button>
+
+			<Button className='btn--primary'>
+				Click here <BsArrowRightCircle />
+			</Button>
+			<Button className='btn--primary-40'>
+				<BsArrowRightCircle />
+				Click here
+			</Button>
 			<Button className='btn--primary-60'>Click here</Button>
 			<Button className='btn--primary-80'>Click here</Button>
 			<Button className='btn--disabled'>Click here</Button>
+
+			<Button className='btn--circle'>
+				<BsArrowRightCircle />
+			</Button>
+			<Button className='btn--circle btn--primary'>
+				<BsArrowRightCircle />
+			</Button>
+			<Button className='btn--circle btn--primary-40'>
+				<BsArrowRightCircle />
+			</Button>
+			<Button className='btn--circle btn--primary-60'>
+				<BsArrowRightCircle />
+			</Button>
+			<Button className='btn--circle btn--primary-80'>
+				<BsArrowRightCircle />
+			</Button>
+			<Button className='btn--circle btn--disabled'>
+				<BsArrowRightCircle />
+			</Button>
 		</>
 	);
 }
